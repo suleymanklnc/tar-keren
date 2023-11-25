4de109e900f6bde8346cca8facdeb3ebff6006d5
süleyman kılınc <suleymanklinc@hotmail.com>
2023-11-26 02:47:46 +0300

    tarık eren portfolyo

---
+++ app.js
@@ -0,0 +1,11 @@
+//loading
+window.addEventListener('load', function () {
+    let loadingScreen = document.getElementById('loading');
+
+    this.setTimeout(function () {
+        loadingScreen.style.display = 'none';
+    }, 3000);
+})
+
+//end of loading
+
